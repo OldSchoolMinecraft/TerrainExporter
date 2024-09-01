78da3eea1ff221797a4dc004f94947927feea0ee
moderatorman <12193288+moderatorman@users.noreply.github.com>
2024-08-31 21:12:03 -0700

    Remove bulk export command

--- src/main/java/net/oldschoolminecraft/te/TerrainExporter.java
+++ src/main/java/net/oldschoolminecraft/te/TerrainExporter.java
@@ -25,27 +25,6 @@ public class TerrainExporter extends JavaPlugin
 
     public void onEnable()
     {
-        getCommand("exportterrain").setExecutor((sender, command, label, args) ->
-        {
-            if (!(sender.hasPermission("te.saveall") || sender.isOp()))
-            {
-                sender.sendMessage(ChatColor.RED + "You don't have permission for this command!");
-                return true;
-            }
-
-            System.out.println("=== STARTING BULK TERRAIN EXPORT ===");
-            new Thread(() ->
-            {
-                try
-                {
-                    this.exportChunkedTerrainData();
-                } catch (Exception ex) {
-                    ex.printStackTrace(System.err);
-                }
-            }).start();
-            return true;
-        });
-
         // Command to export current chunk terrain data
         getCommand("savechunk").setExecutor((sender, command, label, args) ->
         {
@@ -102,6 +81,12 @@ public class TerrainExporter extends JavaPlugin
 
         getCommand("import").setExecutor(((sender, cmd, label, args) ->
         {
+            if (!(sender.hasPermission("te.import") || sender.isOp()))
+            {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+                return true;
+            }
+
             if (args.length < 2)
             {
                 sender.sendMessage("Usage: /import <chunkX> <chunkZ>");
@@ -146,41 +131,42 @@ public class TerrainExporter extends JavaPlugin
         System.out.println("=== BULK TERRAIN EXPORT COMPLETED ===");
     }
 
-    private void exportSingleChunkTerrainData(int chunkX, int chunkZ) throws IOException
-    {
+    private void exportSingleChunkTerrainData(int chunkX, int chunkZ) throws IOException {
         File chunkFile = new File(getDataFolder(), "chunk." + chunkX + "." + chunkZ + ".dat");
         if (!chunkFile.getParentFile().exists()) chunkFile.getParentFile().mkdirs();
         if (!chunkFile.exists()) chunkFile.createNewFile();
         else {
             if (!chunkFile.delete()) chunkFile.deleteOnExit();
-            if (!chunkFile.createNewFile())
-            {
+            if (!chunkFile.createNewFile()) {
                 System.out.println("Failed to manage chunk file: " + chunkX + "," + chunkZ);
                 return;
             }
         }
-        try (DataOutputStream dos = new DataOutputStream(Files.newOutputStream(chunkFile.toPath())))
-        {
-            // Write the starting coordinates of the chunk
-            dos.writeInt(chunkX * CHUNK_SIZE);
-            dos.writeInt(chunkZ * CHUNK_SIZE);
+        try (DataOutputStream dos = new DataOutputStream(Files.newOutputStream(chunkFile.toPath()))) {
+            Chunk chunk = getServer().getWorld("world").getChunkAt(chunkX, chunkZ);
+            if (!chunk.isLoaded()) chunk.load();
 
-            // Loop through all blocks in the chunk
-            for (int x = 0; x < CHUNK_SIZE; x++)
-            {
-                for (int z = 0; z < CHUNK_SIZE; z++)
-                {
+            ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN);
+
+            buffer.putInt(chunkX * CHUNK_SIZE);
+            dos.write(buffer.array());
+
+            buffer.clear();
+            buffer.putInt(chunkZ * CHUNK_SIZE);
+            dos.write(buffer.array());
+
+            for (int x = 0; x < CHUNK_SIZE; x++) {
+                for (int z = 0; z < CHUNK_SIZE; z++) {
                     int worldX = chunkX * CHUNK_SIZE + x;
                     int worldZ = chunkZ * CHUNK_SIZE + z;
                     int highestY = getServer().getWorld("world").getHighestBlockYAt(worldX, worldZ);
-                    Block block = getServer().getWorld("world").getBlockAt(worldX, highestY, worldZ);
-
-                    // Get the block type code
+                    Block block = getServer().getWorld("world").getBlockAt(worldX, highestY - 1, worldZ);
                     int blockTypeCode = block.getTypeId();
 
-                    // Write height and block type to file
                     dos.writeByte(highestY); // Height (1 byte)
-                    dos.writeInt(blockTypeCode); // Block type (4 bytes)
+                    buffer.clear();
+                    buffer.putInt(blockTypeCode);
+                    dos.write(buffer.array()); // Block type (4 bytes)
                 }
             }
             getLogger().info("Chunk " + chunkX + ", " + chunkZ + " exported.");
